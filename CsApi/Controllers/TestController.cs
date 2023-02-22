1960acdf005464400f3b9ed92515ff3657bf455a
Xuitty <ufokun@gmail.com>
2023-02-22 17:53:44 +0800

    Add project files.

---
+++ CsApi/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using CsApi.Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        //[Route("")]
+        [HttpGet]
+        public String TestClass(string? a = null, string? b = null)
+        {
+            if (a != null && b != null)
+            {
+                return "a =" + a + " b = " + b;
+            }
+            else if (a != null)
+            {
+                return "a = " + a;
+            }
+            else if (b != null)
+            {
+                return "b = " + b;
+            }
+            else
+            {
+                Account account = new Account();
+                account.Aaccount = "55688";
+                return JsonSerializer.Serialize(account);
+                //return account.ToString();
+            }
+        }
+        //[HttpGet]
+        //[Route("{a}/{b}")]
+        //public String TestClass1(string a, string b)
+        //{
+        //    return a+b;
+        //}
+    }
+}
---
+++ CsApi/Entity/Account.cs
@@ -0,0 +1,18 @@
+﻿namespace CsApi.Entity
+{
+    public class Account
+    {
+        public int Aid { get; set; }
+        public int Atype { get; set; } = 1;
+        public string Aaccount { get; set; }
+        public Decimal Abalance { get; set; }
+        public int Aactive { get; set; }
+        public string Averify { get; set; }
+        public string Anickname { get; set; }
+        public int uid { get; set; }
+        public int statuss { get; set; }
+        public string message { get; set; }
+
+        
+    }
+}
